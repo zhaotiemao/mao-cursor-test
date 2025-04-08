702121b9f32da2b043a5d56674250222728843b0
luobote <zhao_tiemao@163.com>
2025-04-08 15:09:59 +0800

    Add common constants

---
+++ mao-cursor-test-api/src/main/java/com/mao/api/constant/CommonConstants.java
@@ -0,0 +1,11 @@
+package com.mao.api.constant;
+
+/**
+ * Common constants for the application
+ */
+public interface CommonConstants {
+    String SUCCESS_CODE = "0";
+    String ERROR_CODE = "1";
+    String SUCCESS_MSG = "success";
+    String ERROR_MSG = "error";
+}
