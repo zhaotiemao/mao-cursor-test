feea011edbdad6e7c1f0571751ca13c330fa9840
luobote <zhao_tiemao@163.com>
2025-04-08 15:11:18 +0800

    Add HelloService interface

---
+++ mao-cursor-test-api/src/main/java/com/mao/api/service/HelloService.java
@@ -0,0 +1,14 @@
+package com.mao.api.service;
+
+/**
+ * Example service interface
+ */
+public interface HelloService {
+    /**
+     * Say hello to someone
+     *
+     * @param name the name to say hello to
+     * @return greeting message
+     */
+    String sayHello(String name);
+}
