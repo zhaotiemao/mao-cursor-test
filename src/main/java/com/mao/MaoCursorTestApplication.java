d52c15952fb668d150d4b241f0d919c61d2ec1dd
luobote <zhao_tiemao@163.com>
2025-04-08 15:02:43 +0800

    Add main application class

---
+++ src/main/java/com/mao/MaoCursorTestApplication.java
@@ -0,0 +1,13 @@
+package com.mao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaoCursorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaoCursorTestApplication.class, args);
+    }
+
+}
